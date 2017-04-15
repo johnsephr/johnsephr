7e848957e2328606050e2b714fd262b29fc27c6d
John <John@users-mbp-2.attlocal.net>
2017-04-14 19:05:21 -0500

    Initial Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name scratchApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the scratchApp
+ */
+angular.module('scratchApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
